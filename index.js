193d1c505f572c62c6760eb14add5c517a9eefdb
osxsystem <osxsystem2014@gmail.com>
2021-12-29 09:53:10 +0700

    Update

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-const express = require('express');
 var admin = require("firebase-admin");
-
 var serviceAccount = require("./openfreightone-mobo-firebase-adminsdk-s8xe9-b056181661.json");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://openfreightone-mobo-default-rtdb.firebaseio.com"
+  databaseURL: "https://openfreightone-mobo.firebaseio.com"
 });
 
-/*
 var message = {
   notification: {
-    title: 'Customclearance task',
-    body: 'sdfkshdfkjsdfjksfhkjsdfhksfhjksfhjksday 10 ajhksjfhdsfhshdkfhsdkfjshdkf'
+    title: 'Test from node',
+    body: 'FCM test from NODE'
   },
   data: {
     msgType: 'Task',
     word: 'Hola hola!!!'
   },
   android: {
-    priority: 'high',
     notification: {
       sound: 'default',
-      visibility: 'public'
+      visibility: 'public',
+      priority: 'high',
     }
   },
   apns: {
     payload: {
       aps: {
+        // Required for background/quit data-only messages on iOS
+        contentAvailable: true,
         sound: 'default'
       }
     }
   },
-  token: 'eDlri3jMRyGLUa4LfXTUbD:APA91bH356UgJRAh8VYM7EvyL3F0mE0_fJcJHF3LzsRm0NtULv6L3eWWjuxCLIuK23uzs4156QnI2uWPGeK-6qX0LL8KXnQTAo4aBX5C8btie2mNpyvm4PDVgfuIVEtMkCWydNLI6KeT'
+  token: 'fvHEDTVkjUhzkc3qFVEMk6:APA91bFxLZX_ZCeDIHBf8VYFqR_TvbFSJ2n306j9pBNiSyo0mWKfO5y70J2TdZeSZqvwLY8NcngdRogqKpFlPqmNS_mpo0JFuUxqOHVcHQng8c6HYd6kYwKBDazq5y_aTdNbhL_fo0Dp'
+  // topic: 'customclearance'
 };
-
+// admin.messaging().sendToDevice
 admin.messaging().send(message)
 .then(response => {
   console.log('Successfully sent message: ', response);
@@ -41,33 +42,3 @@ admin.messaging().send(message)
 .catch(error => {
   console.log('Error sending message: ', error);
 });
-console.log(admin);
-*/
-
-const app = express();
-const port = 3000;
-app.use(express.json());
-
-const notification_options = {
-  priority: "high",
-  timeToLive: 60 * 60 * 24
-};
-app.post('/firebase/notification', (req, res) => {
-  const  registrationToken = req.body.registrationToken
-  const message = req.body.message
-  const options =  notification_options
-
-  admin.messaging().sendToDevice(registrationToken, message, options)
-  .then( response => {
-    console.log(response);
-    res.status(200).send("Notification sent successfully")
-  })
-  .catch( error => {
-    console.log(error);
-  });
-
-});
-
-app.listen(port, () => {
-  console.log("listening to port"+port)
-})
